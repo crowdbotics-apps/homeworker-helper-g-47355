b8594508e46a8ddd09ce95acfac9a3fd40977033
Crowdbotics <team@crowdbotics.com>
2024-04-02 13:50:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "homeworker-helper-g-47355",
+  url: "https://homeworker-helper-g-47355.botics.co",
+  api: "https://homeworker-helper-g-47355.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
